f0beba93003db7ef6d0cea5879a21adcb658e613
Aman Jaiswal <antooaman@gmail.com>
2020-04-24 00:05:33 +0530

    Updates

---
+++ precache-manifest.cee1314c9d2dce6d98db550f33993d9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7185d2380d7c666ec994f81a62f162f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bebe7bfcb31646484995",
+    "url": "/monsters-rolodex/static/css/main.ab84e8c6.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bebe7bfcb31646484995",
+    "url": "/monsters-rolodex/static/js/main.3f9390d5.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
